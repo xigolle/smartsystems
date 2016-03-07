ef7cce2322c83ada60fc660be54b2ea395f69497
xigolle <joey.driessen@live.nl>
2016-03-07 22:21:34 +0100

    first setup for program

--- smartsystems.ino
+++ smartsystems.ino
@@ -1,15 +1,36 @@
+int enableLeft = 5;
+int enableRight = 6;
+int inputLeftFront = 0;
+int inputLeftBack = 1;
+int inputRightBack = 10;
+int inputRightFront = 15;
+
 void setup()
 {
-
-  /* add setup code here */
-
-}
-
-void loop()
-{
-
-  /* add main program code here */
-
-
+	//start serial connection
 	
-}
+	Serial.begin(9600);
+	while (!Serial);/*
+	pinMode(enableLeft, OUTPUT);
+	pinMode(enableRight, OUTPUT);
+	pinMode(inputLeftFront, OUTPUT);
+	pinMode(inputLeftBack, OUTPUT);
+	pinMode(inputRightBack, OUTPUT);
+	pinMode(inputRightFront, OUTPUT);
+	/* add setup code here */
+	
+	}
+
+	void loop()
+	{
+		if (Serial.available()) {
+			Serial.print(Serial.read());
+		}
+			Serial.print("Hello arduino");
+
+
+
+
+
+	}
+	
