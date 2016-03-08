af6c533a76041cb7441babea1ca5a2682d818dd2
xigolle <joey.driessen@live.nl>
2016-03-08 22:53:13 +0100

    Add Forward movement car & selection logic

--- __vm/.Smart systems.vsarduino.h
+++ __vm/.Smart systems.vsarduino.h
@@ -5,62 +5,79 @@
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: Arduino Duemilanove or Diecimila w/ ATmega328, Platform=avr, Package=arduino
+	Hardware: Arduino Due (Programming Port), Platform=sam, Package=arduino
 */
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __AVR_ATmega328p__
-#define __AVR_ATmega328P__
-#define F_CPU 16000000L
-#define ARDUINO 166
-#define ARDUINO_AVR_DUEMILANOVE
-#define ARDUINO_ARCH_AVR
+#define printf iprintf
+#define F_CPU 84000000L
+#define ARDUINO 167
+#define ARDUINO_SAM_DUE
+#define ARDUINO_ARCH_SAM
+#define __SAM3X8E__
+#define USB_VID 0x2341
+#define USB_PID 0x003e
+#define USBCON
 #define __cplusplus
-#define __AVR__
+#define __ARM__
 #define __inline__
 #define __asm__(x)
 #define __extension__
-//#define __ATTR_PURE__
-//#define __ATTR_CONST__
+#define __ATTR_PURE__
+#define __ATTR_CONST__
 #define __inline__
-//#define __asm__ 
 #define __volatile__
-#define GCC_VERSION 40801
-#define volatile(va_arg) 
-#define _CONST
-typedef void *__builtin_va_list;
-#define __builtin_va_start
-#define __builtin_va_end
-//#define __DOXYGEN__
-#define __attribute__(x)
-#define NOINLINE __attribute__((noinline))
+#define __SIZE_TYPE__ long
+
+#define __ICCARM__
+#define __ASM
+#define __INLINE
+#define __builtin_va_list void
+//#define _GNU_SOURCE 
+//#define __GNUC__ 0
+//#undef  __ICCARM__
+//#define __GNU__
+
+typedef long Pio;
+typedef long Efc;
+typedef long Adc;
+typedef long Pwm;
+typedef long Rtc;
+typedef long Rtt;
+typedef long pRtc;
+typedef long Spi;
+typedef long spi;
+typedef long Ssc;
+//typedef long p_scc;
+typedef long Tc;
+//typedef long pTc;
+typedef long Twi;
+typedef long Wdt;
+//typedef long pTwi;
+typedef long Usart;
+typedef long Pdc;
+typedef long Rstc;
+
+extern const int ADC_MR_TRGEN_DIS = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG0 = 0;
+extern const int ADC_MR_TRGSEL_Pos = 0;
+
+extern const int ADC_MR_TRGSEL_Msk = 0;
+extern const int ADC_MR_TRGEN = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG1 = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG2 = 0;
+extern const int ADC_MR_TRGSEL_ADC_TRIG3 = 0;
+
+
+
+#define __ARMCC_VERSION 400678
+#define __attribute__(noinline)
+
 #define prog_void
 #define PGM_VOID_P int
-#ifndef __builtin_constant_p
-#define __builtin_constant_p __attribute__((__const__))
-#endif
-#ifndef __builtin_strlen
-#define __builtin_strlen  __attribute__((__const__))
-#endif
-#define NEW_H
-/*
-#ifndef __ATTR_CONST__
-#define __ATTR_CONST__ __attribute__((__const__))
-#endif
 
-#ifndef __ATTR_MALLOC__
-#define __ATTR_MALLOC__ __attribute__((__malloc__))
-#endif
 
-#ifndef __ATTR_NORETURN__
-#define __ATTR_NORETURN__ __attribute__((__noreturn__))
-#endif
-
-#ifndef __ATTR_PURE__
-#define __ATTR_PURE__ __attribute__((__pure__))
-#endif            
-*/
 typedef unsigned char byte;
 extern "C" void __cxa_pure_virtual() {;}
 
@@ -68,6 +85,7 @@ extern "C" void __cxa_pure_virtual() {;}
 
 #include <arduino.h>
 #include <pins_arduino.h> 
+#include <variant.h> 
 #undef F
 #define F(string_literal) ((const PROGMEM char *)(string_literal))
 #undef PSTR
--- smartsystems.ino
+++ smartsystems.ino
@@ -4,33 +4,53 @@ int inputLeftFront = 0;
 int inputLeftBack = 1;
 int inputRightBack = 10;
 int inputRightFront = 15;
-
+char choice;
 void setup()
 {
 	//start serial connection
-	
+
 	Serial.begin(9600);
-	while (!Serial);/*
+	
 	pinMode(enableLeft, OUTPUT);
 	pinMode(enableRight, OUTPUT);
+	/*
 	pinMode(inputLeftFront, OUTPUT);
 	pinMode(inputLeftBack, OUTPUT);
 	pinMode(inputRightBack, OUTPUT);
 	pinMode(inputRightFront, OUTPUT);
-	/* add setup code here */
-	
+	*/
+}
+
+void loop()
+{
+	if (Serial.available()) {
+		Serial.print("Serial is available");
+			//if the choise changed put the new value in the variable
+			choice = Serial.read();
+		
+		
+	}
+	if (choice == '0') {
+		//stop the car
+		Serial.print("Stopping car");
+	}
+	else if (choice == '1') {
+		//drive car forward
+		Serial.print("Driving car forward");
+		carForward(255);
 	}
 
-	void loop()
-	{
-		if (Serial.available()) {
-			Serial.print(Serial.read());
-		}
-			Serial.print("Hello arduino");
 
 
 
+}
 
+void carForward(int speed) {
+	digitalWrite(enableLeft, HIGH);
+	digitalWrite(enableRight, HIGH);
+	digitalWrite(A0, HIGH);
+	digitalWrite(A1, LOW);
+	digitalWrite(A2, HIGH);
+	digitalWrite(A3, LOW);
+}
 
-	}
-	
