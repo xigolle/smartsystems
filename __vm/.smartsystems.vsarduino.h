24ada0726d2675df4f58f00cf4c36bd75ce968f4
xigolle <joey.driessen@live.nl>
2016-03-15 13:40:35 +0100

    fixed fuckup

--- __vm/.smartsystems.vsarduino.h
+++ __vm/.smartsystems.vsarduino.h
@@ -102,5 +102,5 @@ extern "C" void __cxa_pure_virtual() {;}
 #define portOutputRegister(P)
 #define portInputRegister(P)
 #define portModeRegister(P)
-#include <..\smartsystemscar\smartsystemscar.ino>
+#include <..\smartsystems\smartsystems.ino>
 #endif
